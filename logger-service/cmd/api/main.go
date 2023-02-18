8c6dfd418098ee3c2ffc21eaefbea7215d6f5462
kkonat <presto@presto.biz.pl>
2023-02-18 21:14:47 +0100

    add models to config, serve and disconnect

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"log-service/data"
+	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -18,6 +22,7 @@ const (
 var client *mongo.Client
 
 type Config struct {
+	Models data.Models
 }
 
 func main() {
@@ -28,8 +33,33 @@ func main() {
 	}
 
 	client = mongoClient
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	app := Config{
+		Models: data.New(client),
+	}
+
+	go app.serve()
 }
 
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s", webPort),
+		Handler: app.routes(),
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
+
+}
 func connectToMongo() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(mongoURL)
 	clientOptions.SetAuth(options.Credential{
