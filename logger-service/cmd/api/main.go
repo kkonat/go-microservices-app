d25c8644beeca2fa880e63824204221580360a4b
kkonat <presto@presto.biz.pl>
2023-02-20 22:27:31 +0100

    add simple rpc support

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -42,9 +44,13 @@ func main() {
 			panic(err)
 		}
 	}()
+
 	app := Config{
 		Models: data.New(client),
 	}
+
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
 
 	// go app.serve()
 	log.Println("Starting service on port", webPort)
@@ -70,6 +76,24 @@ func (app *Config) serve() {
 	}
 }
 
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			log.Println("logger RPC listen error: ", err)
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+}
 func connectToMongo() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(mongoURL)
 	clientOptions.SetAuth(options.Credential{
